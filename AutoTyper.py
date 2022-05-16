fc44044aacf7478b84c065b427c44ad5d700db17
nuno <75589883+nuno-src@users.noreply.github.com>
2022-05-17 00:59:51 +0100

    Update AutoTyper.py

--- AutoTyper.py
+++ AutoTyper.py
@@ -6,24 +6,47 @@ import random
 
 char_list = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","w","x","y","z"]
 
-c = 68
-m = 75
-l = 120
+words = ["qwe", "qwer", "asd", "as", "zxc", "asdf", "qw", "qqq", "gfjh", "enytu", "ertyj", "luiuykt","wefyguqjn",
+ "dsds", "gfhghf","sfbthyt","wqef","uyil","weqr","dsaf","ewrq","zxc","dghk","asdas","etyj","yuit","jhfk"]
 
 
+# variaveis de tempo
+c = 70
+m = 82
+l = 122
+ll = 215
+
+
+print('''
+
+    _         _        _____
+   / \  _   _| |_ ___ |_   __   _ _ __   ___ _ __
+  / _ \| | | | __/ _ \  | || | | | '_ \ / _ | '__|
+ / ___ | |_| | || (_)   | || |_| | |_) |  __| |
+/_/   \_\__,_|\__\___/  |_| \__, | .__/ \___|_|
+                            |___/|_|
+
+
+
+
+''')
+
+
+
+print("The Auto Typer is running...")
 
 while True:
 
     time.sleep(15)
-    keyboard.write(random.choice(char_list))
+    keyboard.write(random.choice(words))
+    time.sleep(1)
+    keyboard.press("Enter")
+    time.sleep(c)
+    keyboard.write("dfghf")
     time.sleep(1)
     keyboard.press("Enter")
     time.sleep(m)
-    keyboard.write(random.sample(char_list, 2))
-    time.sleep(1)
-    keyboard.press("Enter")
-    time.sleep(m)
-    keyboard.write("qwert")
+    keyboard.write("qwer")
     time.sleep(1)
     keyboard.press("Enter")
     time.sleep(m)
@@ -34,12 +57,28 @@ while True:
     keyboard.write("qw")
     time.sleep(1)
     keyboard.press("Enter")
-    time.sleep(m)
-    keyboard.write("e")
+    time.sleep(ll)
+    keyboard.write("asd")
     time.sleep(1)
     keyboard.press("Enter")
     time.sleep(m)
-    keyboard.write("as")
+    keyboard.write(random.choice(words))
+    time.sleep(1)
+    keyboard.press("Enter")
+    time.sleep(c)
+    keyboard.write(random.choice(char_list))
+    time.sleep(1)
+    keyboard.press("Enter")
+    time.sleep(m)
+    keyboard.write(random.sample(char_list, 2))
+    time.sleep(1)
+    keyboard.press("Enter")
+    time.sleep(m)
+    keyboard.write("er")
+    time.sleep(1)
+    keyboard.press("Enter")
+    time.sleep(m)
+    keyboard.write(random.choice(words))
     time.sleep(1)
     keyboard.press("Enter")
     time.sleep(c)
@@ -51,4 +90,5 @@ while True:
     time.sleep(1)
     keyboard.press("Enter")
     time.sleep(50)
+    print("Restarting cycle...")
 
