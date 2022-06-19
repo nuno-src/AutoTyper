3e63b0db16339835392f58bf00b31f18fc887db9
nuno <75589883+nuno-src@users.noreply.github.com>
2022-06-19 16:27:00 +0100

    Update AutoTyper.py

--- AutoTyper.py
+++ AutoTyper.py
@@ -7,10 +7,11 @@ import random
 char_list = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","w","x","y","z"]
 
 words = ["asd", "as","asdf", "qwe", "qwer", "wreq", "ewrq", "weqr", "qw", "qqq", "gfjh", "enytu", "ertyj", "luiuykt",
-    "wefyguqjn", "dsds", "gfhghf","sfbthyt","wqef","uyil","dsaf","dghk","asdas","etyj","yuit","jhfk", "jkçl","jklç", "jçkl", "wwww",
-    "sadf","hgkj","hjkg", "tyru", "bnm","vbnm", "vbmn", "vcbn", "bnvm","uoiy","uyio","xzvc", "zxc"]
+    "wefyguqjn", "dsds", "gfhghf","sfbthyt","wqef","uyil","dsaf","dghk","asdas","etyj","yuit","jhfk", "jkçl","jklç", "jçkl", "jkçhçkjlk",
+    "çkljhjççjkl", "wwww", "sadf","hgkj","hjkg", "tyru", "bnm","vbnm", "vbmn", "vcbn", "bnvm","uoiy","uyio","xzvc", "zxc"]
 
-
+words2 = ["gfjh", "gfhghf", "hgkj","hjkg", "jhfk", "jkçl","jklç", "jçkl", "jkçhçkjlk", "kjlºçlkºj","hgjlkçl",
+    "çkljhjççjkl", "bnvmv", "bnm", "vbnm", "vbmn", "vcbn", "bnvm", "kjlççkljº","jljkºllçk", "jklçklj"]
 
 # variaveis de tempo
 c = 71
@@ -22,12 +23,15 @@ ll = 215
 
 # Verificar duplicados na lista words
 uwords = []
-
 for i in words:
     if i not in uwords:
         uwords.append(i)
     
-
+    
+uwords2 = []
+for w in words2:
+    if w not in uwords2:
+        uwords2.append(w)
 
 
 
@@ -52,8 +56,8 @@ print("\nThe Auto Typer is running...")
 
 
 
-def random_printing():
-    r = random.choice(words)
+def random_printing(list):
+    r = random.choice(list)
     print(f"printing {r} in 3 seconds...")
     time.sleep(3)
     keyboard.write(r)
@@ -76,43 +80,104 @@ def countDown(s):
         time.sleep(1)
 
 
-countDown(15)
 
 
-while True:
+def o1():
+
+    while True:
+
+        random_printing(words)
+        time.sleep(c)
+        static_printing("dfghf")
+        time.sleep(m)
+        static_printing("qwer")
+        time.sleep(m)
+        static_printing("qw")
+        time.sleep(l)
+        static_printing("qw")
+        time.sleep(ll)
+        static_printing("asd")
+        time.sleep(m)
+        random_printing(words)
+        time.sleep(c)
+        keyboard.write(random.choice(char_list))
+        time.sleep(1)
+        keyboard.press("Enter")
+        time.sleep(m)
+        keyboard.write(random.sample(char_list, 2))
+        time.sleep(1)
+        keyboard.press("Enter")
+        time.sleep(m)
+        static_printing("er")
+        time.sleep(m)
+        random_printing(words)
+        time.sleep(c)
+        static_printing("as")
+        time.sleep(c)
+        keyboard.write(random.choice(char_list))
+        time.sleep(1)
+        keyboard.press("Enter")
+        time.sleep(62)
+        print("\nRestarting cycle...")
 
 
-    random_printing()
-    time.sleep(c)
-    static_printing("dfghf")
-    time.sleep(m)
-    static_printing("qwer")
-    time.sleep(m)
-    static_printing("qw")
-    time.sleep(l)
-    static_printing("qw")
-    time.sleep(ll)
-    static_printing("asd")
-    time.sleep(m)
-    random_printing()
-    time.sleep(c)
-    keyboard.write(random.choice(char_list))
-    time.sleep(1)
-    keyboard.press("Enter")
-    time.sleep(m)
-    keyboard.write(random.sample(char_list, 2))
-    time.sleep(1)
-    keyboard.press("Enter")
-    time.sleep(m)
-    static_printing("er")
-    time.sleep(m)
-    random_printing()
-    time.sleep(c)
-    static_printing("as")
-    time.sleep(c)
-    keyboard.write(random.choice(char_list))
-    time.sleep(1)
-    keyboard.press("Enter")
-    time.sleep(62)
-    print("\nRestarting cycle...")
 
+
+def o2():
+
+    while True:
+        random_printing(uwords2)
+        time.sleep(c)
+        random_printing(uwords2)
+        time.sleep(m)
+        random_printing(uwords2)
+        time.sleep(m)
+        random_printing(uwords2)
+        time.sleep(l)
+        random_printing(uwords2)
+        time.sleep(ll)
+        random_printing(uwords2)
+        time.sleep(m)
+        random_printing(uwords2)
+        time.sleep(m)
+        random_printing(uwords2)
+        time.sleep(m)
+        random_printing(uwords2)
+        time.sleep(m)
+        random_printing(uwords2)
+        time.sleep(c)
+        random_printing(uwords2)
+        time.sleep(c)
+        random_printing(uwords2)
+        time.sleep(62)
+        print("\nRestarting cycle...")
+
+
+
+if __name__ == '__main__':
+
+    opc = ""
+    while opc != "exit":
+
+
+        print("\n===================================================")
+        print("                     Choose an option")
+        print("===================================================")
+        print("  [1] - option 1 ")
+        print("  [2] - option 2(recomended)")
+        print("  [exit]")
+        print("===================================================")
+
+
+        opc = input("Choose an option: ")
+
+        if opc == "1":
+            countDown(15)
+            o1()
+        elif opc == "2":
+            countDown(15)
+            o2()
+        elif opc == "exit":
+            print("\nA sair...")
+        else:
+            print("\nERROR! - INVALID OPTION!")
